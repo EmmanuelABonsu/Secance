3040be0ea12e66b03a94f9f8d6f50d520909f71a
Emmanuel Bonsu <ebonsu2018@gmail.com>
2021-02-23 22:03:50 +0000

    Mobile app code

---
+++ app/src/main/java/com/example/secance/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.secance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        TextView have_account = findViewById(R.id.alreadyHaveAccount);
+        have_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
